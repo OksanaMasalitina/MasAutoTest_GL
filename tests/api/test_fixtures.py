05cf860f4dacf6f9c259408dda002368defb6296
OksanaMasalitina <oksjuice.archive@gmail.com>
2024-01-14 15:57:52 +0200

    Added the Projec_Task1

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,27 +1,11 @@
-class User:
+import pytest
 
-    def __init__(self) -> None:
-        self.name = None
-        self.second_name = None
-    
-    def create(self):
-        self.name = 'Sergii'
-        self.second_name = 'Butenko'
-    
-    def remove(self):
-        self.name = ''
-        self.second_name = ''
-    
-    def test_change_name():
-        user = User()
-        user.create()
 
-        assert user.name == 'Sergii'
-        user.remove()
+@pytest.mark.check
+def test_change_name(user):
+    assert user.name == 'Oksana'
 
-    def test_change_second_name():
-        user = User()
-        user.create()
 
-        assert user.second_name == 'Butenko'
-        user.remove()
+@pytest.mark.check
+def test_change_second_name(user):
+    assert user.second_name == 'Masalitina'
