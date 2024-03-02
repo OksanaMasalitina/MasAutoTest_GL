878af18c8a79aa4d8a26ac22662f3fbd2af060bc
OksanaMasalitina <oksjuice.archive@gmail.com>
2024-03-03 00:30:42 +0200

    UI test_searching_product_and_adding_to_cart added

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -4,7 +4,67 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-#import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+@pytest.mark.ui_golka
+def test_searching_product_and_adding_to_cart():
+    # Створення об'єкту для керування браузером
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    
+    # Відкриття головної сторінки сайту https://golka.com.ua
+    driver.get("https://golka.com.ua")
+
+    # Пошук поля, в яке треба буде ввести назву товару
+    search_elem = driver.find_element(By.ID, "input_search")
+   
+    # Ввід назви товару "Пташка в гнізді"
+    search_elem.send_keys("Пташка в гнізді")
+
+    # Натискання клавіші Enter
+    search_elem.send_keys(Keys.RETURN)
+    
+    #Очікування завантаження результатів пошуку 
+    product_bird_elem = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.XPATH, "//div[@class='rm-module-title']/a[contains(text(), 'Пташка в гнізді')]")))
+
+    # Знаходимо у результатах пошуку потрібний товар
+    product_bird_elem = driver.find_element(By.XPATH, "//div[@class='rm-module-title']/a[contains(text(), 'Пташка в гнізді')]")
+   
+    # Відкриваємо сторінку зі знайденим товаром
+    product_bird_elem.click()
+
+    # Перевірка, що знаходимось на сторінці потрібного товару
+    expected_url = "https://golka.com.ua/beadwork/embroidery-beads/biser-brooch/broshky-byserom/bp-314-brosh-ptichka-v-gnezde-crystal-art-nabor-dlya-vishivki-biserom.html"
+    assert driver.current_url == expected_url
+
+    # Пошук кнопки додавання товару "Пташка в гнізді" до кошику
+    button_cart_elem = driver.find_element(By.ID, "button-cart")
+
+    # Клік кнопки додавання в кошик
+    button_cart_elem.click()
+
+    # Чекати на відображення модального вікна за його класом 
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "modal-title")))
+
+    # Перевірка, що модальне вікно містить кнопку "Оформити замовлення"
+    button_order_elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Оформити замовлення")))
+
+    # Пошук кнопки всередині модального вікна за CSS селектором
+    button_order_elem = driver.find_element(By.LINK_TEXT, 'Оформити замовлення')
+    
+    # Клік на знайдену кнопку
+    button_order_elem.click()
+    
+    # Пауза, щоб насолодитися результатом)
+    time.sleep(3)
+
+    # Закриття браузера
+    driver.close()
+
 
 @pytest.mark.ui 
 def test_check_incorrect_username():
@@ -19,24 +79,23 @@ def test_check_incorrect_username():
 
     # Ввід неправильного імені користувача або емейла
     login_elem.send_keys("sergiibutenko@mistakenemail.com")
-    #time.sleep(3)
 
     # Пошук поля, в яке треба буде ввести неправильний пароль
     pass_elem = driver.find_element(By.ID, "password")
     
     # Ввід неправильного пароля
     pass_elem.send_keys("wrong password")
-    #time.sleep(3)
 
     # Пошук кнопки "Sign in"
     btn_elem = driver.find_element(By.NAME, "commit")
 
-    # Емуляція кліка лівоб кнопкою миши
+    # Емуляція кліка лівою кнопкою миши
     btn_elem.click()
     
     # Перевірка, що назва сторінки така, як очікується
     assert driver.title == "Sign in to GitHub · GitHub"
-    #time.sleep(3)
 
     # Закриття браузера
     driver.close()
+
+
